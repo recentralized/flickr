41bdee9ff2385e03249d5cf01c24f44c76be160f
Ryan Carver <ryan@ryancarver.com>
2018-11-06 17:04:38 -0800

    add SetAllExtrasForGetPhotos for convenience

--- people/people.go
+++ people/people.go
@@ -157,6 +157,12 @@ type GetPhotosOptionalArgs struct {
 	Page          int               // 0 to ignore
 }
 
+// SetAllExtrasForGetPhotos populates the Extras field with all possible
+// values, ensuring that all possible data comes back in the response.
+func SetAllExtrasForGetPhotos(a *GetPhotosOptionalArgs) {
+	a.Extras = "description,license,date_upload,date_taken,owner_name,icon_server,original_format,last_update,geo,tags,machine_tags,o_dims,views,media,path_alias,url_sq,url_t,url_s,url_q,url_m,url_n,url_z,url_c,url_l,url_o"
+}
+
 func GetPhotos(client *flickr.FlickrClient,
 	userId string, opts GetPhotosOptionalArgs) (*PhotoListResponse, error) {
 	client.Init()
